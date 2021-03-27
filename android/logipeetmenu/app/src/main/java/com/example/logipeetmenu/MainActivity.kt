c350bf866b4f59d8ba63f1dbd933748ba046be53
Beatriz Carvas <beatrizcarvas@ua.pt>
2021-03-27 12:15:48 +0000

    qualqer coisa

---
+++ android/logipeetmenu/app/src/main/java/com/example/logipeetmenu/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.logipeetmenu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    // private lateinit var todoAdapter: TodoAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // todoAdapter = TodoAdapeter(mutableListOf())
+        //  rvTodoItems.adapter = tododapter
+        //  rvTodoItems.layoutManager = LinearLayoutManager(context: this)
+
+        // btnAddTodo.setOnClickListener{
+        // val todoTitle= etTodoTitle.text.toString()
+        // if (todoTitle.isNotEmpty()){
+        //  val todo =Todo(todoTitle)
+        //   (...)  --> 1:02 h
+        // }
+        // }
+        // falta função
+    }
+}
---
+++ android/logipeetmenu/app/src/main/java/com/example/logipeetmenu/things.kt
@@ -0,0 +1,6 @@
+package com.example.logipeetmenu
+
+data class things (
+        val title:String,
+        val isChecked: Boolean = false
+)
