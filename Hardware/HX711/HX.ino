19506a89b4bf0e7e01e1e9800f9fc66de33568b0
Guilherme  Cajeira <gcajeira@ua.pt>
2021-04-13 17:11:56 +0000

    Upload New File

---
+++ Hardware/HX711/HX.ino
@@ -0,0 +1,33 @@
+#include <HX711.h>
+
+HX711 scale;              // criamos variável scale do tipo HX711 
+
+//uint8_t DDOUT = A0;      
+//uint8_t SSCK = A1;
+
+void setup() 
+{
+  Serial.begin(9600);
+  
+  scale.begin(A0,A1);
+
+  scale.set_scale(100);
+  //scale.callibrate_scale(500,10);
+  scale.tare();
+  
+  Serial.print("\nScale ");
+  Serial.print(scale.get_scale());
+  Serial.print("\nTara ");
+  Serial.print(scale.get_tare());     // imprime o valor da tara (peso da taça s/comida)
+}
+
+void loop() 
+{
+  //Serial.print("\nScale ");
+ // Serial.print(scale.get_scale());
+  Serial.print("\nPrint ");
+  Serial.print(scale.get_units());
+
+  delay(1000);
+
+}
