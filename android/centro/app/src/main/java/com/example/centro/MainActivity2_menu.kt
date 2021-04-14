1027ee486387752cc9a837bd80827ee11151b26b
Beatriz Carvas <beatrizcarvas@ua.pt>
2021-04-14 11:34:43 +0100

    adicionado pasta centro

---
+++ android/centro/app/src/main/java/com/example/centro/MainActivity2_menu.kt
@@ -0,0 +1,11 @@
+package com.example.centro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2_menu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_activity2_menu)
+    }
+}
