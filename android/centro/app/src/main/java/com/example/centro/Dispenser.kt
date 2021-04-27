4a1f9f3604312c549a8e033dc089ff09aa27e9cf
Tiago <rodriguestiago@ua.pt>
2021-04-28 00:23:39 +0100

    Scanner_de_código_de_barras_e_QR

---
+++ android/centro/app/src/main/java/com/example/centro/Dispenser.kt
@@ -0,0 +1,11 @@
+package com.example.centro
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Dispenser : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dispenser)
+    }
+}
--- android/centro/app/src/main/java/com/example/centro/leitor_codigo.kt
+++ android/centro/app/src/main/java/com/example/centro/leitor_codigo.kt
@@ -1,11 +1,100 @@
 package com.example.centro
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.budiyev.android.codescanner.*
+
+private const val CAMERA_REQUEST_CODE = 101
 
 class leitor_codigo : AppCompatActivity() {
+
+    private lateinit var codeScanner: CodeScanner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_leitor_codigo)
+
+        setupPermissions()
+        codeScanner()
+    }
+
+    private fun codeScanner(){
+        codeScanner = CodeScanner(this, findViewById(R.id.scanner_view))
+
+        codeScanner.apply {
+            camera = CodeScanner.CAMERA_BACK
+            formats = CodeScanner.ALL_FORMATS
+
+            autoFocusMode = AutoFocusMode.SAFE
+            scanMode = ScanMode.CONTINUOUS
+            isAutoFocusEnabled = true
+            isFlashEnabled = false
+
+            decodeCallback = DecodeCallback {
+                runOnUiThread {
+                    // code for value of barcode!!!!
+
+                    
+                    // Test purposes only
+                    //findViewById<TextView>(R.id.tv_textView).text = it.text
+                }
+            }
+
+            errorCallback = ErrorCallback {
+                runOnUiThread {
+                    Log.e("Main", "Camera initialization error: ${it.message}")
+                }
+            }
+        }
+
+        findViewById<CodeScannerView>(R.id.scanner_view).setOnClickListener {
+            codeScanner.startPreview()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+
+    private fun setupPermissions(){
+        val permission = ContextCompat.checkSelfPermission(this,
+        android.Manifest.permission.CAMERA)
+
+        if(permission != PackageManager.PERMISSION_GRANTED){
+            makeRequest()
+        }
+    }
+
+    private fun makeRequest(){
+        ActivityCompat.requestPermissions(this,
+        arrayOf(android.Manifest.permission.CAMERA),
+        CAMERA_REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            CAMERA_REQUEST_CODE -> {
+                if(grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this, "You need the camera permission to be able to use this function!", Toast.LENGTH_SHORT).show()
+                } else {
+                    // successful!
+                }
+            }
+        }
     }
 }
