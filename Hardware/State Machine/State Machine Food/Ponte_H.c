9c475cd1dbbafbf786303d6ff0797c0bcdd23c41
dFernandes15 <d.fernandes15@ua.pt>
2021-05-17 16:26:50 +0000

    Upload New File

---
+++ Hardware/State Machine/State Machine Food/Ponte_H.c
@@ -0,0 +1,55 @@
+#include <Stepper.h>    //This library allows you to control unipolar or bipolar stepper motors.
+#include <SPI.h>
+
+//Inputs do do arduino (neste caso arduino UNO)
+int IN1 = 30;
+int IN2 = 31;
+
+// To stop flag = 0;
+// To rotate flag = 1;
+
+//int enA = 9;
+
+void setup() {
+
+    Serial.begin(9600);
+    SPI.begin();
+    // Set the output pins
+
+    //Motor A
+   // pinMode(enA, OUTPUT);
+    pinMode(IN1, OUTPUT);   
+    pinMode(IN2, OUTPUT);
+
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+}
+
+void main(int Flag) {
+
+    //analogWrite(enA, 255);
+    
+    if(Flag == 1)
+    {
+        // Rotate the Motor A clockwise
+        digitalWrite(IN1, HIGH);
+        digitalWrite(IN2, LOW);
+        Serial.println("Clockwise");
+        delay(2000);
+    }
+    else
+    {
+        // Stopping the Motor A
+        digitalWrite(IN1, HIGH);
+        digitalWrite(IN2, HIGH);
+        Serial.println("Stopping");
+        delay(500);
+    }
+}
+
+//Para o caso do motor B
+
+//Rotação Horário ->        IN3=5V, IN4=ground
+//Rotação Anti-Horário ->   IN3=ground, IN4=5V
+//Ponto Morto ->            IN3=IN4=ground
+//Parado ->                 IN3=IN4=5V
