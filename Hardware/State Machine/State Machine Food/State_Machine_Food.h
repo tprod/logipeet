3e5dfccac08a653c76e8b43ccd14a7ec7d8c27bd
dFernandes15 <d.fernandes15@ua.pt>
2021-05-17 16:28:00 +0000

    Upload New File

---
+++ Hardware/State Machine/State Machine Food/State_Machine_Food.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// Definition of State and Events
+typedef enum {st_Init, st_Disp} sm_state_t;             //Declaration of states
+typedef enum {ev_Time_Disp. ev_Full_Bowl} sm_event_t;   //Declaration of events
+
+// Definition of the state machine structure
+typedef struct {
+    sm_state_t current_state;
+    sm_state_t initial_state; // Não acho que seja preciso
+    sm_event_t last_event;
+} sm_t; 
+
+// Set Initial state
+void sm_init(sm_t *psm);   //psm = pointer state Machine (acho eu...)
+
+// State Machine Execution
+void sm_execute(sm_t *psm);
