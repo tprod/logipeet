328e21d1ac617e6a7e5f06c5aa3c4750279eb36d
Tiago <rodriguestiago@ua.pt>
2021-05-28 10:00:27 +0100

    SplashScreen que vai diretamente para o menu inicial

--- android/centro/app/src/main/java/com/example/centro/splashscreen.kt
+++ android/centro/app/src/main/java/com/example/centro/splashscreen.kt
@@ -1,11 +1,29 @@
 package com.example.centro
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class splashscreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
+
+        findViewById<ImageView>(R.id.logo_doggo).alpha = 0f
+        findViewById<ImageView>(R.id.logo_doggo).animate().setDuration(1500).alpha(1f).withEndAction {
+            val i = Intent(this, MainActivity2_menu::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+
+        findViewById<ImageView>(R.id.logo_name).alpha = 0f
+        findViewById<ImageView>(R.id.logo_name).animate().setDuration(1500).alpha(1f).withEndAction {
+            val i = Intent(this, MainActivity2_menu::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
     }
 }
