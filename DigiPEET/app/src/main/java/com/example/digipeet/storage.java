2aeefb21bd64605631df6b57f5f855082ff514bd
Tiago <rodriguestiago@ua.pt>
2021-06-07 23:48:30 +0100

    Layout do leitor de código de barras e QRcode funcional tal como o seu código, devido a dificuldades na implementação em java e outras razões o código do leitor está em kotlin e a funcionar

--- DigiPEET/app/src/main/java/com/example/digipeet/storage.java
+++ DigiPEET/app/src/main/java/com/example/digipeet/storage.java
@@ -1,7 +1,9 @@
 package com.example.digipeet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,13 +12,25 @@ import android.widget.TextView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-
-
 public class storage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_storage);
+
+        CardView adicionar = findViewById(R.id.addItems);
+        CardView remover = findViewById(R.id.deleteItems);
+        CardView procura = findViewById(R.id.scanItems);
+        CardView inventario = findViewById(R.id.viewInventory);
+
+        adicionar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(storage.this, AddProduct.class));
+            }
+        });
+
+
     }
 }
