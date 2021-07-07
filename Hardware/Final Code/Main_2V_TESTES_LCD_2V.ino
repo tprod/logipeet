1dd7fd3fa17e76539b192cf6101bfaf0d242971e
Filipa Antunes <filipa.antunes23@ua.pt>
2021-07-07 10:44:52 +0000

    Replace Main_2V_TESTES_LCD_2V.ino

--- Hardware/Final Code/Main_2V_TESTES_LCD_2V.ino
+++ Hardware/Final Code/Main_2V_TESTES_LCD_2V.ino
@@ -8,14 +8,14 @@ sm_t food;
 int peso_taca_agua;
 int food_weigth;
 
-/////////// Variáveis RFID /////////////////////
 int RFID_comida_inicial;
 int RFID_comida_cao;
 int Time2Disp = 0;  // Flag reveived from Rasp to Dispense
 String TAG;
 String TAG_armazenada;
 bool RFID_ON = false; //Vem da RASP
-////////////////////////////////////////////////
+
+
 
 void setup() {
   Serial.begin(9600);
@@ -26,6 +26,7 @@ void setup() {
   setup_Ponte_H();
   setup_Sensor_Nivel();
   setup_LCD();
+  setup_RFID();
   
   sm_init(&water, Init_water);
   sm_init(&food, Init_food);
@@ -36,9 +37,10 @@ void setup() {
   minInit2 = 30;
   horasInit3 = 22;
   minInit3 = 15;
-  pesoInit = 450;
+  pesoInit = 40;
 
 }
+
 
 void loop() {
    LCD();
@@ -66,11 +68,23 @@ void loop() {
    
     sm_execute_water(&water);
     sm_execute_food(&food);
-  
-  ///////////////////////////// RFID ///////////////////////////////////////////////////////
+
+    Serial.print("\n\n Peso da taça: ");
+    Serial.print(PesoTaca_Comida());
+    Serial.print("\n Peso da agua: ");
+    Serial.print(PesoTaca_Agua());
+    Serial.print("\n Peso do Reservatório: ");
+    Serial.print(PesoTaca_Reserv());
+    Serial.print("\n Final Reserv: ");
+    Serial.print(Final_weigth_reservoir);
+    Serial.print("\n Peso Init: ");
+    Serial.print(pesoInit);
+
+
+///////////////////////////// RFID ///////////////////////////////////////////////////////
 
   if(RFID_ON){  //Rasp envia esta flag
-      RFID_comida_inicial = 1000; //PesoTaca_Comida();
+      RFID_comida_inicial = PesoTaca_Comida();  //1000; PesoTaca_Comida();
       TAG = RFID();
 
       if(TAG != "-- -- -- --")
@@ -94,4 +108,5 @@ void loop() {
       digitalWrite(LED_BUILTIN, LOW);
     }
     delay(1);
+
 }
